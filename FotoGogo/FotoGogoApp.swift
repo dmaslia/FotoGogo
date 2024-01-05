7ba4ec3921e165f7062a1891d3e723cd97d9e059
dmaslia <dmaslia@umich.edu>
2024-01-05 12:53:56 -0500

    Initial Commit

---
+++ FotoGogo/FotoGogoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FotoGogoApp.swift
+//  FotoGogo
+//
+//  Created by Daniel Maslia on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct FotoGogoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
